c28d4a0ac6c26d34c237006cfb0871a60b59669b
kostasdvor <kostasdvor@mail.ru>
2023-07-16 16:15:38 +0300

    good(2)

--- api.js
+++ api.js
@@ -55,7 +55,7 @@ export const fetchComments = (text, name, token) => {
         {
             method: "POST",
             headers: {
-                Authorization: token,
+                Authorization: token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k',
             },
             body: JSON.stringify({
                 // date: formatDateTime(currentDate),
--- render.js
+++ render.js
@@ -1,7 +1,9 @@
 import { toggleLike, checkCommentFields, addCommentReplyHandlers } from "./main.js";
 import { fetchAndRenderTasks, fetchComments, login } from './api.js';
 
-let token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
+// let token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
+let token;
+
 let logged = false;
 
 let commentAuthor = "";
